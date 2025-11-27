9c23881538cfd787966786f8e619f8e68d432344
mitulpatel2909 <mitulpatel2909@gmail.com>
2025-11-27 12:34:08 +0530

    first commit

---
+++ 1.introduction.py
@@ -0,0 +1,110 @@
+
+
+# print("Hello Python")
+# print("Hello Python 123")
+
+
+
+# variable  
+
+# print(10)
+# print(10)
+
+
+# a=10
+# print(a)
+
+
+# valid variable
+
+# abc=100
+# ABC=100
+# ABCabc=100
+# abc1=10
+# _last_name="django"
+
+# invalid variable
+
+
+# 1abc=10
+# last name = 10
+# last-name=10
+# na%&$me=10
+
+
+
+# comments 
+
+# single line
+
+# dsf
+# dsaf
+# dfs
+# dfs
+
+# multi line
+
+"""
+fsd
+dsaf
+dfa
+dafs
+"""
+
+# datatypes
+
+
+# name=True
+# print(name)
+# print(type(name))
+# name=10
+# print(name)
+# print(type(name))
+
+
+# input function
+
+# number=10
+# print(number)
+
+# number=input("Enter A Number : ")
+# print(number)
+# print(type(number))
+
+# number=int(input("Enter A Number : "))
+# print(number)
+# print(type(number))
+
+
+
+
+# oprators
+
+# 1. Arithmetic
+
+# +,-,*,/,%,**,//
+
+
+# 2. Assignment
+
+# =,+=,-=,*=,/=,**=,//=
+
+
+# 3. Comparison (relational)
+
+# ==,!=,>,<,>=,<=
+
+
+# 4. Logical
+
+# and, or, not
+
+
+# 5. Identity
+
+# is, is not
+
+
+# 6. Membership
+
+# in, not in
---
+++ 2.oprators.py
@@ -0,0 +1,153 @@
+
+
+
+# 1. Arithmetic
+
+# +,-,*,/,%,**,//
+
+
+# 2. Assignment
+
+# =,+=,-=,*=,/=,**=,//=
+
+
+# 3. Comparison (relational)
+
+# ==,!=,>,<,>=,<=
+
+
+# 4. Logical
+
+# and, or, not
+
+
+# 5. Identity
+
+# is, is not
+
+
+# 6. Membership
+
+# in, not in
+
+
+
+# ============================
+
+
+# 1. Arithmetic
+
+
+# +,-,*,/,%,**,//
+
+# print(10+10)
+# print(10-10)
+# print(10*10)
+# print(10/10)
+
+# print(10/2) 
+# print(11%2)
+
+# print(10**3)  
+# print(10//2)  
+
+
+# =================================
+
+# 2. Assignment
+
+# =,+=,-=,*=,/=,**=,//=
+
+# a=10
+# print(a)
+
+# a=10
+# a+=1
+# print(a)
+
+# a=10
+# a-=1
+# print(a)
+
+# a=10
+# a*=1
+# print(a)
+
+# a=10
+# a/=1
+# print(a)
+
+# a=10
+# a//=1
+# print(a)
+
+# a=10
+# a**=2
+# print(a)
+
+# a=10
+# a%=2
+# print(a)
+
+
+# ========================
+
+# 3. Comparison (relational)
+
+# ==,!=,>,<,>=,<=
+
+# print(10==10)
+
+# print(10!=10)
+
+# print(10<10)
+
+# print(100>10)
+
+# print(10<=100)
+# print(1000>=100)
+
+
+# ============================
+
+# 4. Logical
+
+# and, or, not
+
+
+# print(10==10 and 10<=10)
+
+# print(10==10 or 10<10)
+
+# print(not(10==10 or 10<10))
+
+
+# =========================
+
+# 5. Identity
+
+# is, is not
+
+# print(10 is 110)
+# print(10 == 110)
+
+
+# print(10 is not 110)
+# print(10 != 110)
+
+
+# ========================
+
+# 6. Membership
+
+# in, not in
+
+
+# a=1,2,3,4,5,6,7,8,9,10
+
+# print(101 in a)
+# print(10 not in a)
+
+
+
+
